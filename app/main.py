62252ae3d3411cc7fc11cbf0f330a434934b5035
Dima <dima.grabovskiy2000@gmail.com>
2025-05-25 11:24:34 +0300

    solution 1

--- app/main.py
+++ app/main.py
@@ -1,2 +1,42 @@
 class Dictionary:
-    pass
+    def __init__(self, capacity: int = 8) -> None:
+        self.capacity = capacity
+        self.size = 0
+        self.table = [[] for _ in range(self.capacity)]
+        self.load_factor = 2 / 3
+
+    def __hash_index(self, key):
+        return hash(key) % self.capacity
+
+    def __setitem__(self, key, value):
+        if self.size / self.capacity > self.load_factor:
+            self._resize()
+
+        index = self.__hash_index(key)
+        box = self.table[index]
+        for i, (k, v) in enumerate(box):
+            if k == key:
+                box[i] = (key, value)
+                return
+        box.append((key, value))
+        self.size += 1
+
+    def __getitem__(self, key):
+        index = hash(key) % self.capacity
+        box = self.table[index]
+        for (k, v) in box:
+            if k == key:
+                return v
+        raise KeyError(f"Key {key} not found.")
+
+    def __len__(self):
+        return self.size
+
+    def _resize(self):
+        old_table = self.table
+        self.capacity *= 2
+        self.table = [[] for _ in range(self.capacity)]
+        self.size = 0
+        for bucket in old_table:
+            for (k, v) in bucket:
+                self[k] = v
